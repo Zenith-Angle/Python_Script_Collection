ceeb6cf4dddb5e3dcad84a8b6f4c4f1cf256ad9b
lzw <liangzhw21@lzu.edu.cn>
2024-05-25 12:58:51 +0800

    实现利用多个key进行轮流请求

--- 网络/批量请求.py
+++ 网络/批量请求.py
@@ -5,11 +5,11 @@ import random
 import json
 from datetime import datetime
 import os
-from tqdm import tqdm  # Import tqdm for progress bar
+from tqdm import tqdm  # 导入 tqdm 来显示进度条
 
 
 def load_coordinates(excel_path):
-    # Load the Excel file
+    # 从 Excel 文件中加载数据
     df = pd.read_excel(excel_path)
     return df
 
@@ -28,7 +28,7 @@ def make_request(rectangle, api_key):
 
 
 def save_json(data, index, folder_path):
-    # Get current datetime to use in filename
+    # 获取当前日期时间作为文件名
     now = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = f"{now}_{index}.json"
     filepath = os.path.join(folder_path, filename)
@@ -37,17 +37,19 @@ def save_json(data, index, folder_path):
 
 
 def main():
-    excel_path = "C:\\Users\\25830\\OneDrive - oganneson\\开发竞赛\\数据\\路线数据\\道路状况\\corners_and_centroids.xlsx"
-    output_folder = "C:\\Users\\25830\\OneDrive - oganneson\\开发竞赛\\数据\\路线数据\\道路状况\\请求结果"
-    api_key = "bb3a9c2dda05ce08846a2c24357bd0bf"
+    excel_path = r"D:\开发竞赛\数据\路线数据\西宁\西宁网格1.5km_vertices.xlsx"
+    output_folder = r"D:\开发竞赛\数据\路线数据\道路状况\请求结果_1.5km"
+    api_keys = ["bb3a9c2dda05ce08846a2c24357bd0bf", "eb1c2fe24b0043e604d6086b80d4ae23", "61d3b28494eb37472e64606fdef59c51"]  # 在这里添加所有 API keys
 
     df = load_coordinates(excel_path)
 
-    # Use tqdm to display a progress bar
+    # 使用 tqdm 显示进度条
     for index, row in tqdm(df.iterrows(), total=df.shape[0], desc="Processing API requests"):
         rectangle = f"{row['TopLeftX']},{row['TopLeftY']};{row['BottomRightX']},{row['BottomRightY']}"
-        # Random delay to avoid being banned
-        time.sleep(10 + random.uniform(0, 5))
+        # 通过索引和 keys 列表的长度取余来交替使用 keys
+        api_key = api_keys[index % len(api_keys)]
+        # 随机延迟以避免被封禁
+        time.sleep(2 + random.uniform(0, 1))
         response_data = make_request(rectangle, api_key)
         save_json(response_data, row['Index'], output_folder)
         print(f"Request {row['Index']} completed and saved.")
